5fea5dda9cfbcd080cf7d2643601fd2490e44307
Li_Xun <321731029@qq.com>
2025-09-29 11:42:21 +0800

    fix: 移除硬编码的文件上传URL并设置为空字符串

--- config/index.js
+++ config/index.js
@@ -1,3 +1,2 @@
 // export const uploadActionUrl = "/api-cts/cts/wechat/file/upload";
-export const uploadActionUrl =
-  "https://api.ztc.gzhtsk.com/cts/wechat/file/upload";
+export const uploadActionUrl = "";
