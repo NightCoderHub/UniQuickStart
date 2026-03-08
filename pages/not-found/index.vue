d76efb238a1901d504e839e1bd15d35918cc3fd3
Li_Xun <321731029@qq.com>
2026-03-08 14:38:00 +0800

    feat: 添加404页面
    当用户访问不存在的路由时，显示404页面以提供更好的用户体验。

---
+++ pages/not-found/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <view class="not-found-page">
+    <text class="title">404</text>
+    <text class="desc">页面不存在</text>
+  </view>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped>
+.not-found-page {
+  display: flex;
+  flex-direction: column;
+  gap: 20rpx;
+  align-items: center;
+  justify-content: center;
+  min-height: 100vh;
+  background: #f5f9ff;
+}
+
+.title {
+  font-size: 72rpx;
+  font-weight: 700;
+  color: #2b3551;
+}
+
+.desc {
+  font-size: 30rpx;
+  color: #7f8aa7;
+}
+</style>
